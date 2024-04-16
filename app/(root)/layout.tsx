915efe0c7d291cbca5e44bdef3cb4e9afbfc286a
ESSARHTHUMAR <essarthumar177@gmail.com>
2024-04-16 16:19:32 +0530

    Connection-Video calling

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
+import StreamVideoProvider from '@/Providers/StreamClientProviders'
 import React, { Children, ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
     <main>
-        
+        <StreamVideoProvider>
         {children}
+        </StreamVideoProvider>
         
     </main>
   )
